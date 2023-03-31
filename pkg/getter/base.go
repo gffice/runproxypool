86ff875aa8e9cfbb1ccc04943e0f2916dfef5a67
gffice <114347535+gffice@users.noreply.github.com>
2023-03-31 23:10:48 +0800

    Update base.go

--- pkg/getter/base.go
+++ pkg/getter/base.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"sync"
 
-	"github.com/ssrlive/proxypool/pkg/proxy"
-	"github.com/ssrlive/proxypool/pkg/tool"
+	"github.com/gffice/runproxypool/pkg/proxy"
+	"github.com/gffice/runproxypool/pkg/tool"
 )
 
 // functions for getters
